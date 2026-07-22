4d1aafa58cee646c5e7e0dcb5ac617696f479f27
Indra Gunanda <indra.gunanda@gmail.com>
2026-07-22 12:39:40 +0700

    fix: declare package name

--- tests/package.test.ts
+++ tests/package.test.ts
@@ -6,9 +6,11 @@ const packageJsonUrl = new URL('../package.json', import.meta.url);
 
 test('declares Pi extension and skill directories', async () => {
   const pkg = JSON.parse(await readFile(packageJsonUrl, 'utf8')) as {
+    name?: string;
     pi?: { extensions?: string[]; skills?: string[] };
   };
 
+  assert.equal(pkg.name, 'pi-snatch-skills');
   assert.deepEqual(pkg.pi?.extensions, ['./extensions']);
   assert.deepEqual(pkg.pi?.skills, ['./skills']);
 });
